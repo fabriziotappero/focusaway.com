8b87539458060a46b5a1fa76efbae76a9af49fc4
Fabrizio Tappero <fabrizio.tappero@gmail.com>
2017-08-31 13:10:50 +0200

    trying to fix this strange github jekyll problem of not rendering correctly the works secion

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -54,10 +54,10 @@ function  workLoad() {
         newHTML = 'work/'+ newfolder;
 
     // load new page content and, when completed, run the vimeo iframe resize window
-    //$('.project-load').html(spinner).load(newHTML,function(){$('.project-load').fitVids();$('.project-load .container').css("width", "100%");});
-		$('.project-load').load(newHTML);
+    $('.project-load').html(spinner).load(newHTML,function(){$('.project-load').fitVids();$('.project-load .container').css("width", "100%");});
+		//$('.project-load').load(newHTML);
 		$('.project-title').text(newTitle);
-    console.log(newHTML);
+    //console.log(newHTML);
 
   });
 }
